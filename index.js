2b08e4bd35fd2aab23b03d7e25afcb9c727205df
theresa <theresa@iptp.vn>
2024-03-26 10:07:23 +0700

    chore: inital source code

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const cors = require('cors')
+const db = require("./queries");
+const port = 8888;
+
+app.use(cors())
+
+app.use(bodyParser.json());
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use((err, req, res, next) => {
+    console.error('Error:', err.message);
+    res.status(500).send('Internal Server Error');
+});
+
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
+});
+
+app.get("/users", db.getUsers);
+app.get("/users/:id", db.getUserById);
+app.post("/users", db.createUser);
+app.put("/users/:id", db.updateUser);
+app.delete("/users/:id", db.deleteUser);
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
